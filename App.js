285e28056b615b0c4d5fd1c554f3cbc2c3077331
galinaramaniuk <galina.sergarz@gmail.com>
2023-02-06 14:32:24 -0500

    Created ImageInput component to upload images.

--- App.js
+++ App.js
@@ -9,22 +9,45 @@ import Icon from "./app/components/Icon";
 import AppText from "./app/components/AppText";
 import ListProduct from "./app/components/ListProduct";
 import AccountScreen from "./app/screens/AccountScreen";
+import { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+// import * as Permissions from "expo-permissions";
+import ImageInput from "./app/components/ImageInput";
+
+import { Button, Image } from "react-native";
 
 export default function App() {
   // return <WelcomeScreen />;
   // return <LoginScreen />;
   // return <RegisterScreen />;
   // return <ListingProductsScreen />;
-  return <CreatingProductScreen />;
+  // return <CreatingProductScreen />;
   // return <ViewProductScreen />;
   // return <AccountScreen />;
-}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    // const result = await Permissions.askAsync(Permissions.CAMERA);
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library.");
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.assets[0].uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri} />
+    </Screen>
+  );
+}
---
+++ app/components/ImageInput.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+function ImageInput({ imageUri }) {
+  return (
+    <View style={styles.container}>
+      {!imageUri && (
+        <MaterialCommunityIcons name="camera" color={colors.medium} size={40} />
+      )}
+      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    justifyContent: "center",
+    height: 100,
+    overflow: "hidden",
+    width: 100,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
+
+export default ImageInput;
